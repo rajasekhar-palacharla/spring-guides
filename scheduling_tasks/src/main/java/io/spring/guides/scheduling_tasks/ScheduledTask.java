0097f3b9c51cd700e56ca198a4ac01e985058431
Pavel Chernykh <pavel.v.chernykh@gmail.com>
2015-05-23 20:34:26 +0300

    Walk through 'Scheduling Tasks'

---
+++ scheduling_tasks/src/main/java/io/spring/guides/scheduling_tasks/ScheduledTask.java
@@ -0,0 +1,17 @@
+package io.spring.guides.scheduling_tasks;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class ScheduledTask {
+    public static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        System.out.println("The time is now " + SIMPLE_DATE_FORMAT.format(new Date()));
+    }
+}
