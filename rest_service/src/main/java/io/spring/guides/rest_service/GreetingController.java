31e0c4e48609d2943ab3b86a4430705a1caf5349
Pavel Chernykh <pavel.v.chernykh@gmail.com>
2015-05-23 19:00:02 +0300

    Walk through 'Consuming a RESTful Web Service'

--- rest_service/src/main/java/io/spring/guides/rest_service/GreetingController.java
+++ rest_service/src/main/java/io/spring/guides/rest_service/GreetingController.java
@@ -9,7 +9,7 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
-    private static final String template = "Hello, my friend, %s!";
+    private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping(value = "/greeting", method = RequestMethod.GET)
