6fce7ab00e7f3c3de9f2e9a8e3ce38e667a1b93a
Pavel Chernykh <pavel.v.chernykh@gmail.com>
2015-06-05 23:34:52 +0300

    Walk through 'Integrating Data'

---
+++ integration/src/main/java/io/spring/guides/integration/Application.java
@@ -0,0 +1,13 @@
+package io.spring.guides.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class Application {
+    public static void main(String[] args) throws Exception {
+        ConfigurableApplicationContext ctx = new SpringApplication("/blog/integration.xml").run(args);
+        System.out.println("Hit Enter to terminate");
+        System.in.read();
+        ctx.close();
+    }
+}
