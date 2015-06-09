934648a60ebaacc2baac61e5f59e972c7836d4c6
Pavel Chernykh <pavel.v.chernykh@gmail.com>
2015-06-09 14:59:30 +0300

    Walk through 'Handling Form Submission'

---
+++ handling_form_submission/src/main/java/io/spring/guides/handling_form_submission/Greeting.java
@@ -0,0 +1,22 @@
+package io.spring.guides.handling_form_submission;
+
+public class Greeting {
+    private long id;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
