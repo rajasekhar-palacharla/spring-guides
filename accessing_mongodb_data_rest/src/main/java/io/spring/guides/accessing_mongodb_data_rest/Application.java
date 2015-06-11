bea738f43cc95e4f58633cbcc3b49c2c4d2bf957
Pavel Chernykh <pavel.v.chernykh@gmail.com>
2015-06-11 16:42:07 +0300

    Walk through 'Accessing MongoDB Data with REST'

---
+++ accessing_mongodb_data_rest/src/main/java/io/spring/guides/accessing_mongodb_data_rest/Application.java
@@ -0,0 +1,11 @@
+package io.spring.guides.accessing_mongodb_data_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ accessing_mongodb_data_rest/src/main/java/io/spring/guides/accessing_mongodb_data_rest/Person.java
@@ -0,0 +1,26 @@
+package io.spring.guides.accessing_mongodb_data_rest;
+
+import org.springframework.data.annotation.Id;
+
+public class Person {
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
