0da4018f409e44008b801e8bc81a34b7db430fe4
Pavel Chernykh <pavel.v.chernykh@gmail.com>
2015-06-12 15:26:42 +0300

    Walk through 'Producing a SOAP web service'

---
+++ producing_web_service/src/main/java/io/spring/guides/producing_web_service/Application.java
@@ -0,0 +1,11 @@
+package io.spring.guides.producing_web_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
